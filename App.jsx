de0e8bcd40871dc6012fc0d5b5cf5e84873e5315
msvasusrinivas <meghana8374@gmail.com>
2025-05-06 20:50:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import Header from './Header';
+
+import Cards from './srinu';
+import Ecommers from './down';
+
+import './App.css'
+
+
+function App() {
+
+  return (
+
+    <>
+      <div>
+        <Header />
+
+
+
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+
+
+        <Ecommers />
+
+      </div>
+    </>
+  )
+}
+
+
+export default App
---
+++ down.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import footer from 'react'
+
+
+
+export const Ecommers = () => {
+    return (
+        <div>
+
+
+            this is afooter
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam ad sequi nisi, delectus qui ex saepe quae, sit, neque inventore repudiandae illo quis error ipsa. Quis, illum. Harum, vero.</p>
+
+        </div>
+    )
+}
+
+export default Ecommers
